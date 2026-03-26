6c1d84d2451496aae07ffe31adf8f4e108c2cd9e
benard3360-star <obernard377@gmail.com>
2026-03-27 00:11:38 +0300

    chore: temporarily disable EnsureAmlDatasetMiddleware

--- transaction_monitor/settings.py
+++ transaction_monitor/settings.py
@@ -81,7 +81,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "api.middleware.EnsureAmlDatasetMiddleware",
+    # "api.middleware.EnsureAmlDatasetMiddleware",  # temporarily disabled
 ]
 
 # ========================
